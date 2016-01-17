a212b303bff9e762f5d27f80079819d47cf409a2
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-17 11:40:32 +0100

    also commented waves sample

--- sample/src/samples/samples.js
+++ sample/src/samples/samples.js
@@ -1,8 +1,8 @@
 export class Samples {
   configureRouter(config, router) {
     config.map([
-      { name: 'click-counter', route: 'click-counter', moduleId: 'samples/click-counter/index', title: 'click-counter' },
-      { name: 'waves', route: 'waves', moduleId: 'samples/waves/index', title: 'waves' }
+      { name: 'click-counter', route: 'click-counter', moduleId: 'samples/click-counter/index', title: 'click-counter' }
+      // { name: 'waves', route: 'waves', moduleId: 'samples/waves/index', title: 'waves' }
     ]);
     this.router = router;
   }
