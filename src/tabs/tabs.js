583ead6aaff4abf5d6822646911704ba5078e679
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-18 23:04:27 +0100

    feat(tabs): tried to implement tabSelected event

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -1,4 +1,5 @@
 import { customAttribute, bindable, bindingMode, inject } from 'aurelia-framework';
+import { fireMaterializeEvent } from '../common/events'
 
 @customAttribute('md-tabs')
 @inject(Element)
@@ -8,8 +9,19 @@ export class MdTabs {
   }
   attached() {
     $(this.element).tabs();
+    // $('li a', this.element).on('click', this.fireTabSelectedEvent);
+    this.element.querySelectorAll('li a').forEach(a => {
+      a.addEventListener('click', this.fireTabSelectedEvent);
+    });
   }
   detached() {
     // no destroy handler in tabs
+    this.element.querySelectorAll('li a').forEach(a => {
+      a.removeEventListener('click', this.fireTabSelectedEvent);
+    });
+  }
+  fireTabSelectedEvent() {
+    let href = $('li a').attr('href');
+    fireMaterializeEvent(this.element, 'tabSelected', href);
   }
 }
