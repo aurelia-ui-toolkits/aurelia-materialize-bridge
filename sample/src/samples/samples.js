38468687180c5bea4d4b7f004fd140462f46cdc2
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-19 14:13:40 +0100

    doc(tabs): sample page

--- sample/src/samples/samples.js
+++ sample/src/samples/samples.js
@@ -3,6 +3,7 @@ export class Samples {
     config.map([
       { name: 'click-counter', route: 'click-counter', moduleId: 'samples/click-counter/index', title: 'click-counter' },
       { name: 'collapsible', route: 'collapsible', moduleId: 'samples/collapsible/index', title: 'collapsible' },
+      { name: 'tabs', route: 'tabs', moduleId: 'samples/tabs/index', title: 'tabs' },
       { name: 'waves', route: 'waves', moduleId: 'samples/waves/index', title: 'waves' }
     ]);
     this.router = router;
---
+++ sample/src/samples/tabs/index.js
@@ -0,0 +1,14 @@
+import { useView } from 'aurelia-framework';
+
+@useView('shared/showcase.html')
+export class Index {
+  configureRouter(config, router) {
+    config.title = 'Tabs';
+    config.map([
+      { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' }
+      // { route: 'colors', moduleId: './colors', nav: true, title: 'Colors' }
+    ]);
+
+    this.router = router;
+  }
+}
