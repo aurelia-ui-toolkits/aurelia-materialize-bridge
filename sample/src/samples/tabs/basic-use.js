e2459d11d53f103699a7c29353eaa8f7eb1286c8
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-19 17:28:49 +0100

    fix(tabs): style adjustments

--- sample/src/samples/tabs/basic-use.js
+++ sample/src/samples/tabs/basic-use.js
@@ -1,1 +1,10 @@
-export class basicUse { }
+export class BasicUse {
+  attached() {
+    this.tabs.addEventListener('md-on-tabSelected', (e) => {
+      this.logger.log(`tab selected ${e.detail}`);
+    });
+  }
+  // onTabSelected(e) {
+  //   this.logger.log(`tab selected ${e.detail}`);
+  // }
+}
