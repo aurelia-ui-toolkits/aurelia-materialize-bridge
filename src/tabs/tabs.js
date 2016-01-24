e3d918785f1a81a5a6e4e16e0503280d2709911c
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-24 15:29:42 +0100

    refactor(tabs): changed tabs to set css classes

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -1,24 +1,42 @@
 import { customAttribute, bindable, bindingMode, inject } from 'aurelia-framework';
 import { fireEvent, fireMaterializeEvent } from '../common/events';
+import { CssClassSetter } from '../common/cssClassSetter';
 
 @customAttribute('md-tabs')
 @inject(Element)
 export class MdTabs {
   constructor (element) {
     this.element = element;
+    this.classSetter = new CssClassSetter(this.element);
+    this.tabClassSetters = [];
   }
   attached() {
+    this.classSetter.addClasses('tabs');
     $(this.element).tabs();
-    // $('li a', this.element).on('click', this.fireTabSelectedEvent);
-    let children = this.element.querySelectorAll('li a');
-    [].forEach.call(children, a => {
+
+    let children = this.element.querySelectorAll('li');
+    [].forEach.call(children, child => {
+      let setter = new CssClassSetter(child);
+      setter.addClasses('tab');
+      tabClassSetters.push(setter);
+    });
+
+    let childAnchors = this.element.querySelectorAll('li a');
+    [].forEach.call(childAnchors, a => {
       a.addEventListener('click', this.fireTabSelectedEvent.bind(this));
     });
   }
   detached() {
+    this.classSetter.removeClasses('tabs');
+
     // no destroy handler in tabs
-    let children = this.element.querySelectorAll('li a');
-    [].forEach.call(children, a => {
+
+    tabClassSetters.foreach(setter => {
+      setter.removeClasses('tab');
+    });
+    tabClassSetters = [];
+    let childAnchors = this.element.querySelectorAll('li a');
+    [].forEach.call(childAnchors, a => {
       a.removeEventListener('click', this.fireTabSelectedEvent.bind(this));
     });
   }
