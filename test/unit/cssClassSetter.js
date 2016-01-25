392823212ed2b48cd75b8a7bceeefc737de888d1
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-25 20:53:43 +0100

    refactor(tests): cleaned up cssClassSetter test

--- test/unit/cssClassSetter.js
+++ test/unit/cssClassSetter.js
@@ -8,16 +8,16 @@ describe('CssClassSetter', () => {
   let taskQueue;
   let cssClassSetter;
   // beforeAll(() => initialize());
-  initialize();
+  // initialize();
 
   beforeEach(() => {
+    initialize();
     element = document.createElement('a');
     element.classList.add('preserved-class');
     document.body.appendChild(element);
 
     taskQueue = new TaskQueue();
     cssClassSetter = new CssClassSetter(element);
-    expect(cssClassSetter.element).toBeDefined();
   });
 
   it('applies waves-effect class', () => {
@@ -27,22 +27,16 @@ describe('CssClassSetter', () => {
     expect(element.classList.contains('waves-effect')).toBe(true);
   });
 
-  it('removes waves-effect class on detached()', () => {
+  it('removes waves-effect class', () => {
     cssClassSetter.addClasses('waves-effect');
-    taskQueue.flushMicroTaskQueue();
-
     cssClassSetter.removeClasses('waves-effect');
-    taskQueue.flushMicroTaskQueue();
 
     expect(element.classList.contains('waves-effect')).toBe(false);
   });
 
   it('does not remove preserved-class class on detached()', () => {
     cssClassSetter.addClasses(['waves-effect', 'preserved-class']);
-    taskQueue.flushMicroTaskQueue();
-
     cssClassSetter.removeClasses(['waves-effect', 'preserved-class']);
-    taskQueue.flushMicroTaskQueue();
 
     expect(element.classList.contains('waves-effect')).toBe(false);
     expect(element.classList.contains('preserved-class')).toBe(true);
