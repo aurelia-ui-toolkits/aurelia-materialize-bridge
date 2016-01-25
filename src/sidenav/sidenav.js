4fadcf1401a2f5c6c80939ddaa55da647bc17947
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-25 23:43:54 +0100

    feat(sidenav): added sidenav edge and closeOnClick properties

--- src/sidenav/sidenav.js
+++ src/sidenav/sidenav.js
@@ -1,9 +1,11 @@
-import { customElement, inject } from 'aurelia-framework';
+import { bindable, customElement, inject } from 'aurelia-framework';
 
 @customElement('md-sidenav')
 @inject(Element)
 export class MdSidenav {
   static id = 0;
+  @bindable() edge = 'left';
+  @bindable() closeOnClick = true;
 
   constructor(element) {
     this.element = element;
