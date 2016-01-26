1f24f4ecc2bfa8a30da87d37685cb4d99cc2aa93
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-26 17:47:54 +0100

    test(attributes, collapsible): added unit-test

---
+++ test/unit/attributes.js
@@ -0,0 +1,29 @@
+import {getBooleanFromAttributeValue} from 'src/common/attributes';
+
+describe('Attributes', () => {
+  describe('getBooleanFromAttributeValue', () => {
+    it('returns true on true', () => {
+      let value = true;
+      let result = getBooleanFromAttributeValue(value);
+      expect(result).toBe(true);
+    });
+
+    it('returns true on "true"', () => {
+      let value = 'true';
+      let result = getBooleanFromAttributeValue(value);
+      expect(result).toBe(true);
+    });
+
+    it('returns false on false', () => {
+      let value = false;
+      let result = getBooleanFromAttributeValue(value);
+      expect(result).toBe(false);
+    });
+
+    it('returns false on "false"', () => {
+      let value = 'false';
+      let result = getBooleanFromAttributeValue(value);
+      expect(result).toBe(false);
+    });
+  });
+});
