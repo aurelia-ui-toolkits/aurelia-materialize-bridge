0a994cd8740fc02bfcb1176d391a735892ab87a4
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-28 11:50:09 +0100

    feat(checkbox): added disabled and filledIn property, added the ability to assign "null" and display an indeterminate checkbox, added theming to checkboxes

--- src/checkbox/checkbox.js
+++ src/checkbox/checkbox.js
@@ -1,4 +1,6 @@
 import { bindable, bindingMode, customElement, inject } from 'aurelia-framework';
+import { AttributeManager } from '../common/attributeManager';
+import { getBooleanFromAttributeValue } from '../common/attributes';
 
 @customElement('md-checkbox')
 @inject(Element)
@@ -12,19 +14,38 @@ export class MdCheckbox {
 
   constructor(element) {
     this.element = element;
-    this.controlId = MdCheckbox.id++;
+    this.controlId = `md-checkbox-${MdCheckbox.id++}`;
     this.handleChange = this.handleChange.bind(this);
   }
 
   attached() {
+    this.attributeManager = new AttributeManager(this.checkbox);
+    if (getBooleanFromAttributeValue(this.mdFilledIn)) {
+      this.attributeManager.addClasses('filled-in');
+    }
+    if (this.mdChecked === null) {
+      this.checkbox.indeterminate = true;
+    } else {
+      this.checkbox.indeterminate = false;
+    }
+    if (getBooleanFromAttributeValue(this.mdDisabled)) {
+      this.checkbox.disabled = true;
+    }
     this.checkbox.addEventListener('change', this.handleChange);
   }
 
   detached() {
+    this.attributeManager.removeClasses(['filled-in', 'disabled']);
     this.checkbox.removeEventListener('change', this.handleChange);
   }
 
   handleChange() {
     this.mdChecked = this.checkbox.checked;
+  }
+
+  mdDisabledChanged(newValue) {
+    if (this.checkbox) {
+      this.checkbox.disabled = !!newValue;
+    }
   }
 }
