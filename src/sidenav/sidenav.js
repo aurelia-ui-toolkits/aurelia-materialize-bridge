812e84e29e9e022fbf46cbebf4d1b3faa9c1476b
Daniel Bendel <daniel.bendel@gmail.com>
2016-01-28 18:05:06 +0100

    refactor(sidenav): prepare for "fixed" use

--- src/sidenav/sidenav.js
+++ src/sidenav/sidenav.js
@@ -1,11 +1,14 @@
 import { bindable, customElement, inject } from 'aurelia-framework';
+import { getBooleanFromAttributeValue } from '../common/attributes';
+import { AttributeManager } from '../common/attributeManager';
 
 @customElement('md-sidenav')
 @inject(Element)
 export class MdSidenav {
   static id = 0;
-  @bindable() edge = 'left';
   @bindable() closeOnClick = true;
+  @bindable() edge = 'left';
+  @bindable() fixed = false;
 
   constructor(element) {
     this.element = element;
@@ -13,6 +16,23 @@ export class MdSidenav {
   }
 
   attached() {
-    // this.controlId = 'md-sidenav-' + id++;
+    this.attributeManager = new AttributeManager(this.sidenav);
+    if (getBooleanFromAttributeValue(this.fixed)) {
+      this.attributeManager.addClasses('fixed');
+    }
+  }
+
+  detached() {
+    this.attributeManager.removeClasses('fixed');
+  }
+
+  fixedChanged(newValue) {
+    if (this.attributeManager) {
+      if (newValue) {
+        this.attributeManager.addClasses('fixed');
+      } else {
+        this.attributeManager.removeClasses('fixed');
+      }
+    }
   }
 }
