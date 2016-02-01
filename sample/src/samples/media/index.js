fd3200e7e65b521e8a89cb78024577cbb9db1dd1
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-01 21:59:44 +0100

    chore(sample): add slider api sample

--- sample/src/samples/media/index.js
+++ sample/src/samples/media/index.js
@@ -8,7 +8,8 @@ export class Index {
       { route: ['', 'box'], moduleId: './box', nav: true, title: 'Material Box (basic)' },
       { route: 'box-caption', moduleId: './box-caption', nav: true, title: 'Material Box (caption)' },
       { route: 'slider', moduleId: './slider', nav: true, title: 'Slider' },
-      { route: 'slider-fullscreen', moduleId: './slider-fullscreen', nav: true, title: 'Slider (fullscreen)' }
+      { route: 'slider-fullscreen', moduleId: './slider-fullscreen', nav: true, title: 'Slider (fullscreen)' },
+      { route: 'slider-api', moduleId: './slider-api', nav: true, title: 'Slider (API)' }
     ]);
 
     this.router = router;
