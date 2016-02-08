e3a8ff72816b5aa59bd46ce6746166f2a7e46f48
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-08 11:26:42 +0100

    chore(sample): replaced gitter-sidecar

--- sample/src/help/help.js
+++ sample/src/help/help.js
@@ -1,18 +1,6 @@
 // import { computedFrom } from 'aurelia-binding';
 
 export class Help {
-  attached() {
-    this.tabs.addEventListener('md-on-selected', e => {
-      if (e.detail === '#gitter') {
-        this.toggleChat();
-      }
-    });
-  }
-
-  toggleChat() {
-    this.gitter.toggleChat(true);
-  }
-
   scrollToTop() {
     // window.scrollTo(0, 0);
     $('html, body').animate({ scrollTop: 0 });
