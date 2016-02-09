c08d92814d5b192cac85f0fbbcc7817c257b1204
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-09 13:04:52 +0100

    test(toastService): add naive toastService test

---
+++ test/unit/toastService.js
@@ -0,0 +1,27 @@
+import { MdToastService } from 'src/toast/toastService';
+import 'materialize';
+
+describe('MdToastService', () => {
+  let service;
+
+  beforeEach(() => {
+    service = new MdToastService();
+    spyOn(Materialize, 'toast');
+  });
+
+  it('calls Materialize.toast', () => {
+    service.show('hello test', 2000);
+    // expect(Materialize.toast).toHaveBeenCalledWith('hello test', 2000, undefined, Function);
+    expect(Materialize.toast).toHaveBeenCalled();
+  });
+
+  it('resolves a promise when dismissed', () => {
+    let promise = service.show('hello test', 20);
+    promise.then((done) => {
+      expect(true).toBe(true);
+      done();
+    }, () => {
+      done.fail('promise rejected');
+    });
+  });
+});
