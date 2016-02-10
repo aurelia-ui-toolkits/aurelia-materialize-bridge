47f9fd5e3e59fd76c65020b67fb564ac3d2aeaa5
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-10 20:31:43 +0100

    chore(sample): cleanup

--- sample/src/samples/scrollfire/basic-use.js
+++ sample/src/samples/scrollfire/basic-use.js
@@ -1,23 +1,4 @@
 export class BasicUse {
-  attached() {
-    let options = [ ];
-    let self = this;
-    // $('[md-scrollfire-target]', $('.scrollfire-container')).each((i, el)=> {
-    //   options.push({
-    //     selector: `.scrollfire-container [md-scrollfire-target]:nth-of-type(${i + 1})`,
-    //     callback: self.firstTarget
-    //   });
-    // });
-    // $('.scrollfire-container').children().each((i, el) => {
-    //   if ($(el).has('[md-scrollfire-target]')) {
-    //     options.push({
-    //       selector: `.scrollfire-container [md-scrollfire-target]:nth-of-type(${i + 1})`,
-    //       callback: self.firstTarget
-    //     });
-    //   }
-    // });
-    // Materialize.scrollFire(options);
-  }
   firstTarget() {
     Materialize.showStaggeredList(this.list);
   }
