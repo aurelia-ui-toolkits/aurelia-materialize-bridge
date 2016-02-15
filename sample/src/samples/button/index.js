2088a096199a84132af488b589304d6da748c11c
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-15 21:13:36 +0100

    chore(sample): add fixed fab samples

--- sample/src/samples/button/index.js
+++ sample/src/samples/button/index.js
@@ -6,7 +6,8 @@ export class Index {
     config.title = 'Button';
     config.map([
       { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' },
-      { route: 'fab', moduleId: './fab', nav: true, title: 'Floating action button' }
+      { route: 'fab', moduleId: './fab', nav: true, title: 'Floating action button' },
+      { route: 'fab-fixed', moduleId: './fab-fixed', nav: true, title: 'Fixed action button' }
     ]);
 
     this.router = router;
