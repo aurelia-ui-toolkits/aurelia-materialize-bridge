584808f399c5e554277343d73533fc7613b28979
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-17 21:41:38 +0100

    chore(sample): added samples for predefined card sizes

--- sample/src/samples/card/index.js
+++ sample/src/samples/card/index.js
@@ -7,7 +7,8 @@ export class Index {
     config.map([
       { route: ['', 'basic-use'], moduleId: './basic-use', nav: true,  title: 'Basic use' },
       { route: 'image', moduleId: './image', nav: true,  title: 'Image card' },
-      { route: 'reveal', moduleId: './reveal', nav: true,  title: 'Reveal card' }
+      { route: 'reveal', moduleId: './reveal', nav: true,  title: 'Reveal card' },
+      { route: 'sizes', moduleId: './sizes', nav: true,  title: 'Card sizes' }
     ]);
 
     this.router = router;
