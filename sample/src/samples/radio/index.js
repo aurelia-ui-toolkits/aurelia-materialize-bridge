d99dab0d8b4f7c88fd6b2dc25a6655c5633ffd4e
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-18 20:23:49 +0100

    chore(sample): added binding to objects sample

--- sample/src/samples/radio/index.js
+++ sample/src/samples/radio/index.js
@@ -5,7 +5,8 @@ export class Index {
   configureRouter(config, router) {
     config.title = 'Radio Buttons';
     config.map([
-      { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' }
+      { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' },
+      { route: 'objects', moduleId: './objects', nav: true, title: 'Binding to objects' }
     ]);
 
     this.router = router;
---
+++ sample/src/samples/radio/objects.js
@@ -0,0 +1,8 @@
+export class Objects {
+  colors = [
+    { name: 'red', hex: '#ff0000' },
+    { name: 'green', hex: '#00ff00' },
+    { name: 'blue', hex: '#0000ff' }
+  ];
+  selectedColor = this.colors[1];
+}
