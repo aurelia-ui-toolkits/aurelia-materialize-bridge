036c963b24e802c1bfa5e6dcb2405f80e714d72a
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-21 18:33:44 +0100

    chore(sample): changed waves samples to use registry/sample-runner

--- sample/src/samples/waves/index.js
+++ sample/src/samples/waves/index.js
@@ -1,14 +1,21 @@
-import { useView } from 'aurelia-framework';
+import { inject, useView } from 'aurelia-framework';
+import { Registry } from 'shared/registry';
 
+@inject(Registry)
 @useView('shared/showcase.html')
 export class Index {
+  constructor(registry) {
+    this.registry = registry;
+  }
+
   configureRouter(config, router) {
-    config.title = 'Waves';
-    config.map([
-      { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' },
-      { route: 'colors', moduleId: './colors', nav: true, title: 'Colors' }
-    ]);
+    // config.title = 'Waves';
+    // config.map([
+    //   { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' },
+    //   { route: 'colors', moduleId: './colors', nav: true, title: 'Colors' }
+    // ]);
 
     this.router = router;
+    return this.registry.load(config, 'waves');
   }
 }
