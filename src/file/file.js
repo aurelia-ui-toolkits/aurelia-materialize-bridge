b96bd0b12d0864b4546f1303ff34046cce9ed8ee
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-24 21:05:26 +0100

    feat(md-file): added md-multiple property, changed button class to account for color themes

--- src/file/file.js
+++ src/file/file.js
@@ -1,21 +1,29 @@
 import { bindable, customElement } from 'aurelia-templating';
 import { bindingMode } from 'aurelia-binding';
+import { inject } from 'aurelia-dependency-injection';
 import { fireEvent } from '../common/events';
+import { getBooleanFromAttributeValue } from '../common/attributes';
 
 @customElement('md-file')
+@inject(Element)
 export class MdFileInput {
   @bindable() mdCaption = 'File';
+  @bindable({
+    defaultBindingMode: bindingMode.oneTime
+  }) mdMultiple = false;
   @bindable({
     defaultBindingMode: bindingMode.twoWay
   }) mdValue;
 
   _suspendUpdate = false;
 
-  constructor() {
+  constructor(element) {
+    this.element = element;
     this.handleChangeFromNativeInput = this.handleChangeFromNativeInput.bind(this);
   }
 
   attached() {
+    this.mdMultiple = getBooleanFromAttributeValue(this.mdMultiple);
     $(this.filePath).on('change', this.handleChangeFromNativeInput);
   }
 
