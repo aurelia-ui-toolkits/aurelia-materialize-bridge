797d3b1d26aca26879c0a3beed4f9828b6bdec5e
Daniel Bendel <daniel.bendel@gmail.com>
2016-03-14 10:40:17 +0100

    fix(MdToastService): export service directly, make className optional #136

--- src/toast/toastService.js
+++ src/toast/toastService.js
@@ -1,5 +1,5 @@
 export class MdToastService {
-  show(message, displayLength, className) {
+  show(message: string, displayLength: int, className?: string) {
     return new Promise((resolve, reject) => {
       Materialize.toast(message, displayLength, className, () => {
         resolve();
