4003fe75d723d5354275b6faba723516403f7f33
Daniel Bendel <daniel.bendel@gmail.com>
2016-03-16 21:46:13 +0100

    chore(sample): added html5-validation samples

---
+++ sample/src/samples/input/html5-validation.js
@@ -0,0 +1,4 @@
+export class Html5Validation {
+  password = '';
+  email = '';
+}
--- sample/src/samples/input/index.js
+++ sample/src/samples/input/index.js
@@ -8,6 +8,7 @@ export class Index {
       { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' },
       { route: 'textarea', moduleId: './textarea', nav: true, title: 'Textarea' },
       { route: 'input-types', moduleId: './input-types', nav: true, title: 'Input types' },
+      { route: 'html5-validation', moduleId: './html5-validation', nav: true, title: 'Html5 Validation' },
       { route: 'char-counter', moduleId: './char-counter', nav: true, title: 'Character counter' }
     ]);
 
