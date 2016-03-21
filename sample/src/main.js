b83bb7fc8fda8e0b0d6e5d4b841242e22d58540a
Daniel Bendel <daniel.bendel@gmail.com>
2016-03-21 22:30:52 +0100

    chore(sample): added collection samples

--- sample/src/main.js
+++ sample/src/main.js
@@ -14,6 +14,7 @@ export function configure(aurelia) {
         .useCheckbox()
         .useChip()
         .useCollapsible()
+        .useCollection()
         .useColors()
         .useDatePicker()
         .useDropdown()
