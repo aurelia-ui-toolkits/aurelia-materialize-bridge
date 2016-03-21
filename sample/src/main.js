4d73abcd9b8891be8f15b47e19bd6cd3222d10eb
Daniel Bendel <daniel.bendel@gmail.com>
2016-03-21 21:29:16 +0100

    chore(sample): added footer samples

--- sample/src/main.js
+++ sample/src/main.js
@@ -19,6 +19,7 @@ export function configure(aurelia) {
         .useDropdown()
         .useFab()
         .useFile()
+        .useFooter()
         .useInput()
         .useModal()
         .useNavbar()
