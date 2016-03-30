5d1e8d4252c9a80c43d428d0851465493753b739
Daniel Bendel <daniel.bendel@gmail.com>
2016-03-30 21:50:51 +0200

    chore(sample): add modal callback demo

--- sample/src/samples/modals/basic-use.js
+++ sample/src/samples/modals/basic-use.js
@@ -1,1 +1,17 @@
-export class BasicUse { }
+import { inject } from 'aurelia-framework';
+import { MdToastService } from 'aurelia-materialize-bridge/toast/toastService';
+
+@inject(MdToastService)
+export class BasicUse {
+  constructor(toast) {
+    this.toast = toast;
+  }
+
+  agree(e) {
+    this.toast.show('You agreed!', 4000);
+  }
+
+  disagree(e) {
+    this.toast.show('You disagreed!', 4000);
+  }
+}
