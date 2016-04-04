de451b84150c33b79e4901f2c9f764e19d89408c
Daniel Bendel <daniel.bendel@gmail.com>
2016-04-04 21:46:27 +0200

    chore(sample): add event sample

---
+++ sample/src/samples/modals/events.js
@@ -0,0 +1,21 @@
+import { inject } from 'aurelia-framework';
+import { MdToastService } from 'aurelia-materialize-bridge/toast/toastService';
+
+@inject(MdToastService)
+export class Events {
+  constructor(toast) {
+    this.toast = toast;
+  }
+
+  agree(e) {
+    this.toast.show('You agreed!', 4000);
+  }
+
+  disagree(e) {
+    this.toast.show('You disagreed!', 4000);
+  }
+
+  onComplete(e) {
+    this.logger.log('modal complete');
+  }
+}
