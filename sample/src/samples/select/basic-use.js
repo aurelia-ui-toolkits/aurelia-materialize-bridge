09d3bec5c64dfaab393b845d1198a3d6563bf02e
Daniel Bendel <daniel.bendel@gmail.com>
2016-06-06 10:15:49 +0200

    chore(sample): add enable/disable sample

--- sample/src/samples/select/basic-use.js
+++ sample/src/samples/select/basic-use.js
@@ -1,6 +1,12 @@
 export class BasicUse {
+  disabled = false;
   selectedValue = '';
+
   setSelectedValue() {
     this.selectedValue = 'item3';
+  }
+
+  toggle() {
+    this.disabled = !this.disabled;
   }
 }
