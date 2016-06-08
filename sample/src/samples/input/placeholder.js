36d115db94b0b74ca94374685a57c0f3d83d9609
Daniel Bendel <daniel.bendel@gmail.com>
2016-06-08 21:50:35 +0200

    chore(sample): add text area placeholder sample

--- sample/src/samples/input/placeholder.js
+++ sample/src/samples/input/placeholder.js
@@ -1,6 +1,12 @@
 export class Placeholder {
   textValue = '';
+  textAreaValue = '';
+
   setText() {
     this.textValue = 'something';
+  }
+
+  setTextArea() {
+    this.textAreaValue = 'something';
   }
 }
