372c5e9eba8bf066fc637d873519a9d9ef65f26c
Daniel Bendel <daniel.bendel@gmail.com>
2016-07-22 10:54:49 +0200

    chore(sample): add programmatic close sample to modal

---
+++ sample/src/samples/modals/manual-close.js
@@ -0,0 +1,13 @@
+export class ManualClose {
+  closeDialog = false;
+
+  constructor(toast) {
+    this.toast = toast;
+  }
+
+  agree(e) {
+    if (this.closeDialog) {
+      $(this.myDialog).closeModal();
+    }
+  }
+}
