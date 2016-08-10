a4b636762820156930c9bd53049e2467efdff93f
Daniel Bendel <daniel.bendel@gmail.com>
2016-08-10 20:16:26 +0200

    chore(sample): add html tooltip sample

--- sample/src/samples/dialogs/tooltip-html.js
+++ sample/src/samples/dialogs/tooltip-html.js
@@ -1,1 +1,13 @@
-export class TooltipHtml { }
+export class TooltipHtml {
+  htmlTooltip = `
+  <table>
+    <tr>
+      <td><img src="images/chips-sample-1.jpg" /></td>
+      <td>
+        custom html with crappy table layout<br />
+        but shows that it's working (I mean tables)
+      </td>
+    </tr>
+  </table>
+  `;
+}
