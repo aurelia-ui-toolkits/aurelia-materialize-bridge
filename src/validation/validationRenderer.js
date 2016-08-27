b59785604be4b13958503b8bf000d91140b7dc7e
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-08-27 12:02:30 +0200

    fix(validationRenderer): remove unused classes
    ..valid and invalid on label + lint fix

--- src/validation/validationRenderer.js
+++ src/validation/validationRenderer.js
@@ -17,34 +17,33 @@ export class MaterializeFormValidationRenderer {
     let errorMessage = error.message || 'error';
 
     switch (target.tagName) {
-      case 'MD-INPUT': {
-        let input = target.querySelector('input');
-        if (input) {
-          input.classList.remove('valid');
-          input.classList.add('invalid');
+    case 'MD-INPUT': {
+      let input = target.querySelector('input');
+      if (input) {
+        input.classList.remove('valid');
+        input.classList.add('invalid');
 
-          // focus target
-          error.target = input;
+        // focus target
+        error.target = input;
 
-          let label:any = target.querySelector('label');
-          if (label) {
-            label.classList.remove('valid');
-            label.classList.add('active');
-            label.classList.add('invalid');
+        let label:any = target.querySelector('label');
+        if (label) {
+          label.classList.add('active');
 
-            // get error message from label
-            let msg = label.getAttribute('data-error');
-            if(!msg) {
-              // error message not set? add
-              label.setAttribute('data-error', errorMessage);
-            } else {
-              // set label message into error object
-              error.message = msg;
-            }
+          // get error message from label
+          let msg = label.getAttribute('data-error');
+          if(!msg) {
+            // error message not set? add
+            label.setAttribute('data-error', errorMessage);
+          } else {
+            // set label message into error object
+            error.message = msg;
           }
         }
-        break;
       }
+      break;
+    }
+    default: break;
     }
 
     // tag the element so we know we rendered into it.
@@ -56,25 +55,18 @@ export class MaterializeFormValidationRenderer {
     if (!target || !target.errors || !target.errors.has(error)) {
       return;
     }
-
     target.errors.delete(error);
 
     switch (target.tagName) {
-      case 'MD-INPUT': {
-        let input = target.querySelector('input');
-        if (input) {
-
-          input.classList.remove('invalid');
-          input.classList.add('valid');
-
-          let label:any = target.querySelector('label');
-          if (label) {
-            label.classList.remove('invalid');
-            label.classList.add('valid');
-          }
-        }
-        break;
+    case 'MD-INPUT': {
+      let input = target.querySelector('input');
+      if (input) {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
       }
+      break;
+    }
+    default: break;
     }
   }
 
