edf5522a5739847ea0242a1b7f3f4a672fc3330e
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-06 07:38:54 +0200

    chore(sample): add select validation sample

---
+++ sample/src/samples/select/validation.js
@@ -0,0 +1,31 @@
+import { inject, NewInstance } from 'aurelia-framework';
+import { ValidationController, ValidationRules } from 'aurelia-validation';
+import { MaterializeFormValidationRenderer } from 'aurelia-materialize-bridge';
+
+@inject(NewInstance.of(ValidationController))
+export class Validation {
+  selectedValue = '';
+
+  controller = null;
+
+  rules = ValidationRules
+    .ensure('selectedValue')
+      .required()
+      .rules;
+
+
+  constructor(controller: ValidationController) {
+    this.controller = controller;
+    this.controller.addRenderer(new MaterializeFormValidationRenderer());
+  }
+
+  validateModel() {
+    this.controller.validate().then(v => {
+      if (v.length === 0) {
+        this.message = 'All is good!';
+      } else {
+        this.message = 'You have errors!';
+      }
+    });
+  }
+}
