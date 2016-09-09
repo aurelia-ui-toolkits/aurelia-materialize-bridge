277f7cfa174e74cd4f3839df7e269ba0daee5047
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-09 12:59:28 +0200

    chore(sample): rename project status to components

--- sample/src/app.js
+++ sample/src/app.js
@@ -9,7 +9,7 @@ export class App {
     config.map([
       { name: 'about',            route: ['', 'about'],                moduleId: 'about/about',                 title: 'About' },
       { name: 'installation',     route: 'installation',               moduleId: 'installation/installation',   title: 'Installation' },
-      { name: 'project-status',   route: 'project-status',             moduleId: 'project-status/controls',     title: 'Project status'},
+      { name: 'project-status',   route: 'project-status',             moduleId: 'project-status/controls',     title: 'Components'},
       { name: 'help',             route: 'help',                       moduleId: 'help/help',                   title: 'Help' },
       // { name: 'docs',             route: 'help/docs/:category/:file',  moduleId: 'help/help',                   title: 'Help' },
       { name: 'support',          route: 'help/support',  moduleId: 'help/support',                   title: 'Support' },
