e9fcaf77979a12e8f8e1437625d358ecf12ded7f
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-14 20:28:42 +0200

    chore(sample): apply main.js from #263

--- sample/src/main.js
+++ sample/src/main.js
@@ -1,4 +1,4 @@
-import 'materialize';
+// import 'materialize';
 
 //Configure Bluebird Promises.
 (Promise.config || P.config)({
@@ -8,58 +8,60 @@ import 'materialize';
 });
 
 export function configure(aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .developmentLogging()
-    .plugin('aurelia-materialize-bridge', plugin => {
-      plugin.useClickCounter()
-        .useAutoComplete()
-        .useBadge()
-        .useBreadcrumbs()
-        .useBox()
-        .useButton()
-        .useCard()
-        .useCarousel()
-        .useCharacterCounter()
-        .useCheckbox()
-        .useChip()
-        .useCollapsible()
-        .useCollection()
-        .useColors()
-        .useDatePicker()
-        .useDropdown()
-        .useFab()
-        .useFile()
-        .useFooter()
-        .useInput()
-        .useModal()
-        .useNavbar()
-        .usePagination()
-        .useParallax()
-        .useProgress()
-        .usePushpin()
-        .useRadio()
-        .useRange()
-        .useScrollfire()
-        .useScrollSpy()
-        .useSelect()
-        .useSidenav()
-        .useSlider()
-        .useSwitch()
-        .useTabs()
-        .useTooltip()
-        .useTransitions()
-        .useWaves()
-        .useWell();
-    })
-    .plugin('aurelia-validation');
+  return System.import('materialize').then(() => {
+    aurelia.use
+      .standardConfiguration()
+      .developmentLogging()
+      .plugin('aurelia-materialize-bridge', plugin => {
+        plugin.useClickCounter()
+          .useAutoComplete()
+          .useBadge()
+          .useBreadcrumbs()
+          .useBox()
+          .useButton()
+          .useCard()
+          .useCarousel()
+          .useCharacterCounter()
+          .useCheckbox()
+          .useChip()
+          .useCollapsible()
+          .useCollection()
+          .useColors()
+          .useDatePicker()
+          .useDropdown()
+          .useFab()
+          .useFile()
+          .useFooter()
+          .useInput()
+          .useModal()
+          .useNavbar()
+          .usePagination()
+          .useParallax()
+          .useProgress()
+          .usePushpin()
+          .useRadio()
+          .useRange()
+          .useScrollfire()
+          .useScrollSpy()
+          .useSelect()
+          .useSidenav()
+          .useSlider()
+          .useSwitch()
+          .useTabs()
+          .useTooltip()
+          .useTransitions()
+          .useWaves()
+          .useWell();
+      })
+      .plugin('aurelia-validation');
 
-  aurelia.use.globalResources('shared/collapse-panel');
-  aurelia.use.globalResources('shared/markdown');
-  aurelia.use.globalResources('shared/logger');
-  aurelia.use.globalResources('shared/au-code');
-  aurelia.use.globalResources('shared/sampleValueConverters');
+    aurelia.use.globalResources('shared/collapse-panel');
+    aurelia.use.globalResources('shared/markdown');
+    aurelia.use.globalResources('shared/logger');
+    aurelia.use.globalResources('shared/au-code');
+    aurelia.use.globalResources('shared/sampleValueConverters');
 
-  aurelia.start()
-    .then(au => au.setRoot('app'));
+    return aurelia.start()
+      .then(au => au.setRoot('app'));
+  });
 }
