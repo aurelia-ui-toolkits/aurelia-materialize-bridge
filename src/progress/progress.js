bf7aaeb3a9850240202bef945f1ae4fce8262431
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-18 11:57:57 +0200

    feat(md-progress): allow size changes, close #274

--- src/progress/progress.js
+++ src/progress/progress.js
@@ -7,6 +7,10 @@ import { inject } from 'aurelia-dependency-injection';
 export class MdProgress {
   @bindable() mdColor = null;
   @bindable({
+    defaultBindingMode: bindingMode.twoWay
+  }) mdPixelSize = null;
+  @bindable() mdSize = 'big';
+  @bindable({
     defaultBindingMode: bindingMode.oneTime
   }) mdType = 'linear';
   @bindable({
@@ -20,4 +24,24 @@ export class MdProgress {
   // mdValueChanged(newValue, oldValue) {
   //   console.log('mdValueChanged, newValue:', JSON.stringify(newValue), 'oldValue:', JSON.stringify(oldValue));
   // }
+
+  mdSizeChanged(newValue) {
+    this.mdPixelSize = null;
+    if (this.wrapper) {
+      this.wrapper.style.height = '';
+      this.wrapper.style.width = '';
+    }
+  }
+
+  mdPixelSizeChanged(newValue) {
+    if (isNaN(newValue)) {
+      this.mdPixelSize = null;
+    } else {
+      this.mdSize = '';
+      if (this.wrapper) {
+        this.wrapper.style.height = `${newValue}px`;
+        this.wrapper.style.width = `${newValue}px`;
+      }
+    }
+  }
 }
