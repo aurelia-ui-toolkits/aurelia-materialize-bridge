a75112a2b7e8eb9d261b9d471fa83a822e8da36e
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-21 12:13:42 +0200

    chore(sample): use init delay (#268) in sample

--- sample/src/samples/media/carousel.js
+++ sample/src/samples/media/carousel.js
@@ -1,1 +1,17 @@
-export class Carousel { }
+export class Carousel {
+  images = [];
+
+  attached() {
+    this.images = [
+      { url: 'http://lorempixel.com/250/250/nature/1' },
+      { url: 'http://lorempixel.com/250/250/nature/2' },
+      { url: 'http://lorempixel.com/250/250/nature/3' },
+      { url: 'http://lorempixel.com/250/250/nature/4' }
+    ];
+  }
+
+  addImage() {
+    let nextImage = this.images.length + 1;
+    this.images.push({ url: `http://lorempixel.com/250/250/nature/${nextImage}`});
+  }
+}
