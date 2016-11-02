3e45a148a30cb1531d36f4b0e9fa4afcb2e6787d
Daniel Bendel <daniel.bendel@gmail.com>
2016-11-02 23:00:14 +0100

    chore(sample): remove dropdown fix usage

--- sample/src/main.js
+++ sample/src/main.js
@@ -29,7 +29,7 @@ export function configure(aurelia) {
           .useColors()
           .useDatePicker()
           .useDropdown()
-          .useDropdownFix()
+          // .useDropdownFix()
           .useFab()
           .useFile()
           .useFooter()
