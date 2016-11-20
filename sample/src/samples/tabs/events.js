af9a439ab8dbd2a4cd9f6be0347019177328b2a0
Daniel Bendel <daniel.bendel@gmail.com>
2016-11-20 21:26:17 +0100

    show(sample): add onShow event samples

--- sample/src/samples/tabs/events.js
+++ sample/src/samples/tabs/events.js
@@ -2,4 +2,8 @@ export class Events {
   onTabSelected(e) {
     this.logger.log(`tab selected ${e.detail}`);
   }
+
+  onShow(element) {
+    this.logger.log(`onShow on element: ${element.attr('id')}`);
+  }
 }
