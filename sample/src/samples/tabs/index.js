c54ecd7252f9eb13e2ffa9e83f4bbfea1a446c7b
Daniel Bendel <daniel.bendel@gmail.com>
2016-11-20 20:52:48 +0100

    chore(sample): add tabs in navbar samples

--- sample/src/samples/tabs/index.js
+++ sample/src/samples/tabs/index.js
@@ -7,7 +7,8 @@ export class Index {
     config.map([
       { route: ['', 'basic-use'], moduleId: './basic-use', nav: true, title: 'Basic use' },
       { route: 'events', moduleId: './events', nav: true, title: 'Events' },
-      { route: 'api', moduleId: './api', nav: true, title: 'API' }
+      { route: 'api', moduleId: './api', nav: true, title: 'API' },
+      { route: 'tabs-in-navbar', moduleId: './tabs-in-navbar', nav: true, title: 'Tabs in NavBar' }
     ]);
 
     this.router = router;
