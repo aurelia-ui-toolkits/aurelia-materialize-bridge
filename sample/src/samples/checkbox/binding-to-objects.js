7bc059acd15a7e2179a418e155ea879c1c06da21
Daniel Bendel <daniel.bendel@gmail.com>
2016-11-24 22:02:06 +0100

    chore(all): fix some linter problems

--- sample/src/samples/checkbox/binding-to-objects.js
+++ sample/src/samples/checkbox/binding-to-objects.js
@@ -2,7 +2,7 @@ export class BindingToObjects {
   products = [
     { id: 0, name: 'Motherboard' },
     { id: 1, name: 'CPU' },
-    { id: 2, name: 'Memory' },
+    { id: 2, name: 'Memory' }
   ];
 
   selectedProducts = [];
