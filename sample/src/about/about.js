d6c9cc9f0eed67d3d7219384c176c1e957cf7c86
Daniel Bendel <daniel.bendel@gmail.com>
2016-11-24 20:45:59 +0100

    chore(sample): increase version

--- sample/src/about/about.js
+++ sample/src/about/about.js
@@ -21,7 +21,7 @@ export class About {
   attached() {
     // let bridge = System.get(System.normalizeSync('aurelia-materialize-bridge'));
     // this.version = bridge.version;
-    this.version = '0.20.0';
+    this.version = '0.20.1';
   }
 
   onSelectionChanged(e) {
