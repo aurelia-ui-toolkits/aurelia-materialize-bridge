4e1fb1b1aa2d49beb5c16316d0379308b6b4d354
Daniel Bendel <daniel.bendel@gmail.com>
2016-12-04 15:47:11 +0100

    feat(md-breadcrumbs): allow for custom router

--- src/breadcrumbs/breadcrumbs.js
+++ src/breadcrumbs/breadcrumbs.js
@@ -1,5 +1,5 @@
-import { customElement } from 'aurelia-templating';
-import { inject } from 'aurelia-dependency-injection';
+import {bindable, customElement} from 'aurelia-templating';
+import {inject} from 'aurelia-dependency-injection';
 import {Router} from 'aurelia-router';
 
 // taken from: https://github.com/heruan/aurelia-breadcrumbs
@@ -7,13 +7,32 @@ import {Router} from 'aurelia-router';
 @customElement('md-breadcrumbs')
 @inject(Element, Router)
 export class MdBreadcrumbs {
+  @bindable() router;
+
   constructor(element, router) {
     this.element = element;
+    this.aureliaRouter = router;
+    // this._childRouter = router;
+    // while (router.parent) {
+    //   router = router.parent;
+    // }
+    // this.router = router;
+  }
+
+  bind() {
+    if (!this.router) {
+      this.router = this.aureliaRouter;
+    }
+    let router = this.router;
     this._childRouter = router;
     while (router.parent) {
       router = router.parent;
     }
     this.router = router;
+  }
+
+  routerChanged() {
+    console.log('[breadcrumbs]', this.router);
   }
 
   navigate(navigationInstruction) {
