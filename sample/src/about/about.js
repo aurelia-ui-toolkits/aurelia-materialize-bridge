32dc3e221e364daf0e1638747857590aad1fc302
Daniel Bendel <daniel.bendel@gmail.com>
2017-01-09 21:09:51 +0100

    chore(sample): bump darn version in about -.-

--- sample/src/about/about.js
+++ sample/src/about/about.js
@@ -21,7 +21,7 @@ export class About {
   attached() {
     // let bridge = System.get(System.normalizeSync('aurelia-materialize-bridge'));
     // this.version = bridge.version;
-    this.version = '0.20.6';
+    this.version = '0.21.0';
   }
 
   onSelectionChanged(e) {
