f3825d49452d08107a00351b244dad3eaaa3162f
Daniel Bendel <daniel.bendel@gmail.com>
2017-07-10 11:01:50 +0200

    fix(md-colors): fix text field accent color, ref #449

--- src/colors/md-colors.js
+++ src/colors/md-colors.js
@@ -4,5 +4,5 @@ export class MdColors {
   @bindable() mdPrimaryColor;
   @bindable() mdAccentColor;
   @bindable() mdErrorColor = '#F44336';
-  @bindable() mdSuccessColor;
+  @bindable() mdSuccessColor = '#26a69a';
 }
