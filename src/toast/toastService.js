116067a77beb5da7284f6474964c483e2325604b
Daniel Bendel <daniel.bendel@gmail.com>
2017-08-01 17:12:16 +0200

    feat(md-toast-service): show returns an instance of Toast This instance can be used to call the new remove method

--- src/toast/toastService.js
+++ src/toast/toastService.js
@@ -5,8 +5,8 @@ export class MdToastService {
 
   show(message, displayLength, className?) {
     return new Promise((resolve, reject) => {
-      Materialize.toast(message, displayLength, className, () => {
-        resolve();
+      const toastInstance = Materialize.toast(message, displayLength, className, () => {
+        resolve(toastInstance);
       });
     });
   }
