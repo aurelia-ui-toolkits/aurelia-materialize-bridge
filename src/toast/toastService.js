bd38cc9984bbbd360a1d7ad798ff9435cda7bb31
Daniel Bendel <daniel.bendel@gmail.com>
2017-08-01 17:08:03 +0200

    feat(md-toast-service): add removeAll, ref #459

--- src/toast/toastService.js
+++ src/toast/toastService.js
@@ -1,4 +1,8 @@
 export class MdToastService {
+  removeAll() {
+    Materialize.Toast.removeAll();
+  }
+
   show(message, displayLength, className?) {
     return new Promise((resolve, reject) => {
       Materialize.toast(message, displayLength, className, () => {
