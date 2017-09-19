c960d9622b3c8eff45b6ead24c78145d65d74920
Daniel Bendel <daniel.bendel@gmail.com>
2017-09-19 13:42:48 +0200

    chore(all): add bundle info to importer

--- au-import/importer.js
+++ au-import/importer.js
@@ -1,4 +1,7 @@
 const Container = require('aurelia-dependency-injection').Container;
+const logger = require('aurelia-logging').getLogger('materialize-bridge-importer');
+const fs = require('fs');
+const nodePath = require('path');
 
 let Importer = class {
   static inject() { return [Container, 'package', 'project']; }
@@ -14,16 +17,25 @@ let Importer = class {
   }
 
   execute() {
+    // logger.debug doesn't do anything
     console.log('--- ex importer');
     // console.log(this.project);
     console.log('--- apply patch');
     this.project.applyPatch([
       {
-        "op": "replace",
-        "path": "/build/loader/plugins/0/stub",
-        "value": false
+        'op': 'replace',
+        'path': '/build/loader/plugins/0/stub',
+        'value': false
       }
     ]);
+
+    console.log('--- write bundle info');
+    const bundleInfoFile = nodePath.join(__dirname, './materialize-bundle.json');
+    const buffer = fs.readFileSync(bundleInfoFile);
+    const bundleInfo = buffer.toString();
+    const bundle = JSON.parse(bundleInfo);
+    this.project.addBundle(bundle);
+
     this.project.writeAureliaJSON();
   }
 };
