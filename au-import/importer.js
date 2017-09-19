27bf9bfe4d41787b0cb9934463bf4b40ca3b73db
Daniel Bendel <daniel.bendel@gmail.com>
2017-09-19 14:17:26 +0200

    chore(importer): install jquery and materialize, call AMD script

--- au-import/importer.js
+++ au-import/importer.js
@@ -2,6 +2,8 @@ const Container = require('aurelia-dependency-injection').Container;
 const logger = require('aurelia-logging').getLogger('materialize-bridge-importer');
 const fs = require('fs');
 const nodePath = require('path');
+const { spawn } = require('child_process');
+const npm = require('npm');
 
 let Importer = class {
   static inject() { return [Container, 'package', 'project']; }
@@ -16,11 +18,13 @@ let Importer = class {
     return true;
   }
 
-  execute() {
-    // logger.debug doesn't do anything
-    console.log('--- ex importer');
-    // console.log(this.project);
-    console.log('--- apply patch');
+  async execute() {
+    await new Promise(resolve => {
+      npm.load(this.package.packageJSONPath, () => {
+        resolve();
+      });
+    });
+    logger.info('apply patch');
     this.project.applyPatch([
       {
         'op': 'replace',
@@ -29,12 +33,31 @@ let Importer = class {
       }
     ]);
 
-    console.log('--- write bundle info');
+    logger.info('write bundle info');
     const bundleInfoFile = nodePath.join(__dirname, './materialize-bundle.json');
     const buffer = fs.readFileSync(bundleInfoFile);
     const bundleInfo = buffer.toString();
     const bundle = JSON.parse(bundleInfo);
     this.project.addBundle(bundle);
+
+    logger.info('copy AMD scripts');
+    const materializeFile = nodePath.join(__dirname, '../build/tools/materialize-css.js');
+    const rBuildFile = nodePath.join(__dirname, '../build/tools/rbuild.js');
+    const materialize = fs.readFileSync(materializeFile);
+    fs.writeFileSync('./materialize-css.js', materialize.toString());
+    const rBuild = fs.readFileSync(rBuildFile);
+    fs.writeFileSync('./rbuild.js', rBuild.toString());
+
+    logger.info('execute AMD scripts');
+    await new Promise((resolve, reject) => {
+      npm.commands.install(['materialize-css', 'jquery'], error => {
+        if (error) reject(error);
+        else resolve();
+      });
+    });
+    let rjs = spawn('sh', ['./node_modules/.bin/r.js', '-o', 'rbuild.js']);
+    rjs.stderr.on('data', (data) => { logger.error(data); });
+    rjs.stdout.on('data', (data) => { logger.info(data); });
 
     this.project.writeAureliaJSON();
   }
