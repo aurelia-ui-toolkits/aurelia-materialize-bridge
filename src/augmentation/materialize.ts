8d0c88f09a55f442158835d3b72157433a29f42e
Max <m.balaganskiy@gmail.com>
2018-04-27 21:38:13 +1000

    Upgrade scrollspy to v1

--- src/augmentation/materialize.ts
+++ src/augmentation/materialize.ts
@@ -313,6 +313,35 @@ declare namespace M {
 		 */
 		prev();
 	}
+
+	interface ScrollSpyOptions {
+		/**
+		 * Throttle of scroll handler
+		 * @default 100
+		 */
+		throttle: number;
+
+		/**
+		 * Offset for centering element when scrolled to
+		 * @default 200
+		 */
+		scrollOffset: number;
+
+		/**
+		 * Class applied to active elements
+		 * @default 'active'
+		 */
+		activeClass: string;
+
+		/**
+		 * Used to find active element
+		 * @default id => 'a[href="#' + id + '"]'
+		 */
+		getActiveElement: (id: string) => string;
+	}
+
+	class ScrollSpy extends Component<ScrollSpyOptions> {
+	}
 }
 
 interface JQuery {
