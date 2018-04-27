80ee2f29fb224066d6adbf15947a6f86d21c64a3
Max <m.balaganskiy@gmail.com>
2018-04-27 14:15:04 +1000

    Fixes #510

--- src/toast/toastService.ts
+++ src/toast/toastService.ts
@@ -2,14 +2,15 @@ declare var Materialize: any;
 
 export class MdToastService {
 	removeAll() {
-		Materialize.Toast.removeAll();
+		M.Toast.dismissAll();
 	}
 
-	show(message: string, displayLength?: number, className?: string) {
-		return new Promise((resolve, reject) => {
-			const toastInstance = Materialize.toast(message, displayLength, className, () => {
-				resolve(toastInstance);
-			});
+	show(message: string, displayLength?: number, className?: string, activationPercent?: number, inDuration?: number, outDuration?: number): Promise<M.Toast> {
+		let options: Partial<M.ToastOptions> = { html: message, displayLength, classes: className, activationPercent, inDuration, outDuration };
+		options = JSON.parse(JSON.stringify(options));
+		return new Promise(resolve => {
+			options.completeCallback = () => resolve(instance);
+			let instance = new M.Toast(options);
 		});
 	}
 }
