3589f87014165b06ab82d122b00b521fe4541b8a
Max <m.balaganskiy@gmail.com>
2018-04-30 11:09:39 +1000

    wip #465
    Upgrade pushpin to v1

--- src/augmentation/materialize.ts
+++ src/augmentation/materialize.ts
@@ -346,6 +346,39 @@ declare namespace M {
 	class Range extends Component<undefined> {
 	}
 
+	interface PushpinOptions {
+		/**
+		 * The distance in pixels from the top of the page where the element becomes fixed
+		 * @default 0
+		 */
+		top: number;
+
+		/**
+		 * The distance in pixels from the top of the page where the elements stops being fixed
+		 * @default Infinity
+		 */
+		bottom: number;
+
+		/**
+		 * The offset from the top the element will be fixed at
+		 * @default 0
+		 */
+		offset: number;
+
+		/**
+		 * Callback function called when pushpin position changes. You are provided with a position string
+		 * @default null
+		 */
+		onPositionChange: (this: Pushpin, position: "pinned" | "pin-top" | "pin-bottom") => void;
+	}
+
+	class Pushpin extends Component<PushpinOptions> {
+		/**
+		 * Original offsetTop of element
+		 */
+		originalOffset: number;
+	}
+
 	function textareaAutoResize(textarea: Element): void;
 }
 
