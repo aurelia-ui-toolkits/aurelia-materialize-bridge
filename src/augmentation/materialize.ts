7660317da68f9b45916c7f0b85ed3e6f9761ca79
Max <m.balaganskiy@gmail.com>
2018-04-30 16:17:33 +1000

    wip #465
    Upgrade collapsible to v1

--- src/augmentation/materialize.ts
+++ src/augmentation/materialize.ts
@@ -610,6 +610,64 @@ declare namespace M {
 		onCloseEnd: (this: Modal, el: Element) => void;
 	}
 
+	interface CollapsibleOptions {
+		/**
+		 * If accordion versus collapsible
+		 * @default true
+		 */
+		accordion: boolean;
+
+		/**
+		 * Transition in duration in milliseconds.
+		 * @default 250
+		 */
+		inDuration: number;
+
+		/**
+		 * Transition out duration in milliseconds.
+		 * @default 250
+		 */
+		outDuration: number;
+
+		/**
+		 * Callback function called before modal is opened
+		 * @default null
+		 */
+		onOpenStart: (this: Modal, el: Element) => void;
+
+		/**
+		 * Callback function called after modal is opened
+		 * @default null
+		 */
+		onOpenEnd: (this: Modal, el: Element) => void;
+
+		/**
+		 * Callback function called before modal is closed
+		 * @default null
+		 */
+		onCloseStart: (this: Modal, el: Element) => void;
+
+		/**
+		 * Callback function called after modal is closed
+		 * @default null
+		 */
+		onCloseEnd: (this: Modal, el: Element) => void;
+	}
+
+	class Collapsible extends Component<CollapsibleOptions> {
+		/**
+		 * Open collapsible section
+		 * @param n Nth section to open
+		 */
+		open(n: number);
+
+		/**
+		 * Close collapsible section
+		 * @param n Nth section to close
+		 */
+		close(n: number);
+	}
+
 	function textareaAutoResize(textarea: Element): void;
 }
 
