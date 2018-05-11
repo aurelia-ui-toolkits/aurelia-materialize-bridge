34c55f9ac46316623da5a0ddebce872e4f5ac014
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2018-05-11 16:16:17 +1000

    fix(tabs): fix focused tab header color

--- src/colors/md-colors.ts
+++ src/colors/md-colors.ts
@@ -1,4 +1,5 @@
 import * as au from "../aurelia";
+import { shadeBlendConvert } from "./shade-blend-convert";
 
 @au.customElement("md-colors")
 export class MdColors {
@@ -13,4 +14,14 @@ export class MdColors {
 
 	@au.bindable
 	successColor: string = "#26A69A";
+
+	toRgb(hex: string, lightenDarken?: number) {
+		if (lightenDarken) {
+			hex = shadeBlendConvert(0.3 * lightenDarken, hex);
+		}
+		let r = parseInt(hex.substring(1, 3), 16);
+		let g = parseInt(hex.substring(3, 5), 16);
+		let b = parseInt(hex.substring(5), 16);
+		return `${r},${g},${b}`;
+	}
 }
