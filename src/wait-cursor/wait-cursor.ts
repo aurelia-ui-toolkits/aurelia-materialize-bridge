99bb6a5f1d1fe3ccaa73a60ef9c94ab6bfc9cd6e
Max <m.balaganskiy@gmail.com>
2018-05-15 19:47:07 +1000

    fix(wait-cursor): remove element on detached

--- src/wait-cursor/wait-cursor.ts
+++ src/wait-cursor/wait-cursor.ts
@@ -5,6 +5,8 @@
 export class MdWaitCursorCustomAttribute {
 	constructor(private element: Element, private templatingEngine: au.TemplatingEngine) { }
 
+	progress: HTMLElement;
+
 	value: boolean;
 	valueChanged(newVal: boolean) {
 		if (newVal && this.trResizeDelegate) {
@@ -28,74 +30,77 @@ export class MdWaitCursorCustomAttribute {
 			return;
 		}
 
-		let progress: Element = document.createElement("div");
-		progress.innerHTML = "<md-progress type='circular' size='small' show.bind='value' style='position: absolute; left: 100%; transform: translateX(-100%);'></md-progress>";
-		progress = progress.firstElementChild;
+		this.progress = document.createElement("div");
+		this.progress.innerHTML = "<md-progress type='circular' size='small' show.bind='value' style='position: absolute; left: 100%; transform: translateX(-100%);'></md-progress>";
+		this.progress = this.progress.firstElementChild as HTMLElement;
 
-		inputField.insertAdjacentElement("afterbegin", progress);
-		const view = this.templatingEngine.enhance(progress);
+		inputField.insertAdjacentElement("afterbegin", this.progress);
+		const view = this.templatingEngine.enhance(this.progress);
 		view.bind(this);
 		view.attached();
 	}
 
 	attachedButton() {
-		const progress = document.createElement("div");
-		progress.innerHTML =
+		this.progress = document.createElement("div");
+		this.progress.innerHTML =
 			"<div show.bind='value' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; opacity: 0.7; background: white; z-index: 98;'></div>" +
 			"<md-progress type='circular' size='small' show.bind='value' style='position: absolute; left: 50%; top: 50%; height: 36px; transform: translateX(-50%) translateY(-50%); z-index: 99;'></md-progress>";
-		progress.style.position = "relative";
-		progress.style.display = "inline-block";
-		progress.classList.add("button-wait-cursor-wrapper");
-		this.element.insertAdjacentElement("beforebegin", progress);
-		const view = this.templatingEngine.enhance(progress);
+		this.progress.style.position = "relative";
+		this.progress.style.display = "inline-block";
+		this.progress.classList.add("button-wait-cursor-wrapper");
+		this.element.insertAdjacentElement("beforebegin", this.progress);
+		const view = this.templatingEngine.enhance(this.progress);
 		view.bind(this);
 		view.attached();
-		progress.appendChild(this.element);
+		this.progress.appendChild(this.element);
 	}
 
 	attachedLi() {
-		let progress: Element = document.createElement("div");
-		progress.innerHTML =
+		this.progress = document.createElement("div");
+		this.progress.innerHTML =
 			"<div show.bind='value' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; opacity: 0.7; background: white; z-index: 98; overflow: hidden;'>" +
 			"<md-progress type='circular' size='small' show.bind='value' style='position: absolute; left: 50%; top: 50%; height: 36px; transform: translateX(-50%) translateY(-50%); z-index: 99;'></md-progress>" +
 			"</div>";
-		progress = progress.firstElementChild;
-		let view = this.templatingEngine.enhance(progress);
+		this.progress = this.progress.firstElementChild as HTMLElement;
+		let view = this.templatingEngine.enhance(this.progress);
 		view.bind(this);
 		view.attached();
-		this.element.appendChild(progress);
+		this.element.appendChild(this.progress);
 	}
 
 	attachedTr() {
 		let tr = this.element as HTMLTableRowElement;
 		let firstTd = this.element.firstElementChild;
-		let progress = document.createElement("div");
-		progress.innerHTML =
+		this.progress = document.createElement("div");
+		this.progress.innerHTML =
 			"<div show.bind='value'>" +
 			"<div style='opacity: 0.7; background: white; width: 100%; height: 100%;'></div>" +
 			"<md-progress type='circular' size='small' style='position: absolute; left: 50%; top: 50%; height: 36px; transform: translateX(-50%) translateY(-50%);'></md-progress>" +
 			"</div>";
-		progress = progress.firstChild as HTMLDivElement;
+		this.progress = this.progress.firstChild as HTMLDivElement;
 		this.trResizeDelegate = () => {
 			if (!this.value) {
 				return;
 			}
-			progress.style.position = "absolute";
-			progress.style.top = `${tr.offsetTop}px`;
-			progress.style.left = `${tr.offsetLeft + tr.parentElement.scrollLeft}px`;
-			progress.style.width = `${tr.offsetWidth}px`;
-			progress.style.height = `${tr.offsetHeight}px`;
+			this.progress.style.position = "absolute";
+			this.progress.style.top = `${tr.offsetTop}px`;
+			this.progress.style.left = `${tr.offsetLeft + tr.parentElement.scrollLeft}px`;
+			this.progress.style.width = `${tr.offsetWidth}px`;
+			this.progress.style.height = `${tr.offsetHeight}px`;
 		};
-		let view = this.templatingEngine.enhance(progress);
+		let view = this.templatingEngine.enhance(this.progress);
 		view.bind(this);
 		view.attached();
 		this.trResizeDelegate();
 		window.addEventListener("resize", this.trResizeDelegate);
-		progress.onclick = (ev) => { ev.cancelBubble = true; };
-		firstTd.appendChild(progress);
+		this.progress.onclick = (ev) => { ev.cancelBubble = true; };
+		firstTd.appendChild(this.progress);
 	}
 
 	detached() {
+		if (this.progress) {
+			this.progress.remove();
+		}
 		if (this.trResizeDelegate) {
 			window.removeEventListener("resize", this.trResizeDelegate);
 			this.trResizeDelegate = null;
