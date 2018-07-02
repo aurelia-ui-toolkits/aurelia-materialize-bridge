f6d471603e830f079d2fb7625e45292b2380779f
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2018-07-02 11:46:38 +1000

    fix(all): polyfill remove for IE11
    fixes #517

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,19 @@ export function configure(frameworkConfiguration: FrameworkConfiguration, config
 // build-index-remove start
 export * from "./exports";
 // build-index-remove end
+
+function remove(this: Element) {
+	if (this.parentNode) {
+		this.parentNode.removeChild(this);
+	}
+}
+
+// polyfill remove for IE11
+(function() {
+	if (!Element.prototype.remove) {
+		Element.prototype.remove = remove;
+	}
+	if (Text && !Text.prototype.remove) {
+		Text.prototype.remove = remove;
+	}
+})();
