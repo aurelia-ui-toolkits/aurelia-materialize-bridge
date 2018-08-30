6eb112ea539681a1511ce75b36c5ceac693560e5
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2018-08-31 09:14:35 +1000

    fix(colors): add padStart polyfill for IE11

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import "./augmentation/materialize";
 import { Dummy } from "./augmentation/aurelia-typed-observable";
 import "./polyfills/append";
 import "./polyfills/remove";
+import "./polyfills/pad-start";
 import { FrameworkConfiguration } from "aurelia-framework";
 import { ConfigBuilder } from "./config-builder";
 
