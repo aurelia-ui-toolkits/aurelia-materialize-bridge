ef1ad8e29f643c96ddeac8ed384f5104057f83d1
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2018-12-11 16:27:47 +1100

    fix(select): split command for IE compatibility

--- src/select/select.ts
+++ src/select/select.ts
@@ -74,7 +74,8 @@ export class MdSelect {
 		}
 
 		this.inputField = document.createElement("div");
-		this.inputField.classList.add("input-field", "md-select");
+		this.inputField.classList.add("input-field");
+		this.inputField.classList.add("md-select");
 		let va = this.element.getAttributeNode("validate");
 		if (va) {
 			this.inputField.setAttribute(va.name, va.value);
