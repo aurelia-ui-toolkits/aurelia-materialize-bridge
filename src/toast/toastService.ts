86499880f4e360e09f245c0594b1be46eec946d7
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2019-04-17 14:18:39 +1000

    fix(toastService): rename parameter
    Fixes #546

--- src/toast/toastService.ts
+++ src/toast/toastService.ts
@@ -5,8 +5,8 @@ export class MdToastService {
 		M.Toast.dismissAll();
 	}
 
-	show(message: string, displayLength?: number, className?: string, activationPercent?: number, inDuration?: number, outDuration?: number): Promise<M.Toast> {
-		let options: Partial<M.ToastOptions> = { html: message, displayLength, classes: className, activationPercent, inDuration, outDuration };
+	show(html: string, displayLength?: number, className?: string, activationPercent?: number, inDuration?: number, outDuration?: number): Promise<M.Toast> {
+		let options: Partial<M.ToastOptions> = { html, displayLength, classes: className, activationPercent, inDuration, outDuration };
 		au.cleanOptions(options);
 		return new Promise(resolve => {
 			options.completeCallback = () => resolve(instance);
